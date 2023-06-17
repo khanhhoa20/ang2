1351bac7182359f94687b00f4a7a0af89cad0887
ManhProduce <thieusymanh2001@gmail.com>
2023-06-17 17:18:15 +0700

    add customer service

--- src/app/service/customer.service.ts
+++ src/app/service/customer.service.ts
@@ -1,9 +1,26 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
-export class CustomerService {
+export class CustomerService{
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+  url = "http://localhost:9090/tpbank"
+  loginUrl = "customer/login"
+  cusDetailUrl = "customer/cusdetail"
+
+  login(username: string, password: string): Observable<string> {
+    return this.http.post(`${this.url}/${this.loginUrl}`, {
+      "userName": username,
+      "userPass": password
+    },
+      { responseType: 'text' }
+    )
+  }
+
+
+  
 }
