0367cf21d1c19f1b1bbfe940db1eed8ec09005ef
dat-vo-63 <trieuvo191@gmail.com>
2023-06-20 21:28:58 +0700

    check

--- src/app/operators/view-customers/view-customers.component.spec.ts
+++ src/app/operators/view-customers/view-customers.component.spec.ts
@@ -1,5 +1,4 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { ViewCustomersComponent } from './view-customers.component';
 
 describe('ViewCustomersComponent', () => {
